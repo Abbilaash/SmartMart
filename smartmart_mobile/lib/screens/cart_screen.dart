b45af3d4921a910d24fb45d4abcd20236e07055a
Swadhitha <sp.swadhitha@gmail.com>
2025-09-03 09:56:49 +0530

    login page included

--- smartmart_mobile/lib/screens/cart_screen.dart
+++ smartmart_mobile/lib/screens/cart_screen.dart
@@ -68,7 +68,7 @@ class CartScreen extends StatelessWidget {
             color: Colors.white,
             boxShadow: [
               BoxShadow(
-                color: Colors.grey.withOpacity(0.1),
+                color: Colors.grey.withValues(alpha: 0.1),
                 spreadRadius: 1,
                 blurRadius: 4,
                 offset: const Offset(0, -2),
